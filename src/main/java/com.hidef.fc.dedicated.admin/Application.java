5ac804e0bc140a08fc74cad162b38072c98c4b2a
Robert Stiff <uatecuk@gmail.com>
2016-02-09 22:49:10 +0000

    Bring the CORS filter to the front of the stack so it always get executed no matter what.

--- src/main/java/com.hidef.fc.dedicated.admin/Application.java
+++ src/main/java/com.hidef.fc.dedicated.admin/Application.java
@@ -1,17 +1,35 @@
 package com.hidef.fc.dedicated.admin;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+import javax.servlet.Filter;
 
 @Configuration
 @ComponentScan("com.hidef.fc.dedicated.admin,com.auth0")
 @ImportResource("classpath:auth0-security-context.xml")
 @SpringBootApplication
 public class Application {
+
+    @Bean
+    public SimpleCORSFilter simpleCORSFilter()
+    {
+        return new SimpleCORSFilter();
+    }
+
+    @Bean
+    public FilterRegistrationBean securityFilterChain(SimpleCORSFilter securityFilter) {
+        FilterRegistrationBean registration = new FilterRegistrationBean(securityFilter);
+        registration.setOrder(Integer.MIN_VALUE);
+        return registration;
+    }
 
     public static void main(String[] args) throws InterruptedException {
         SpringApplication.run(Application.class, args);
