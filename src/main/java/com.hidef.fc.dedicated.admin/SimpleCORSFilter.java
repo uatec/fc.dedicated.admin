19b64073f6cd8a4e46a9654f2cb0e229508513c2
Robert Stiff <uatecuk@gmail.com>
2016-02-09 22:51:12 +0000

    Just simplify the CORS filter.

--- src/main/java/com.hidef.fc.dedicated.admin/SimpleCORSFilter.java
+++ src/main/java/com.hidef.fc.dedicated.admin/SimpleCORSFilter.java
@@ -1,13 +1,19 @@
 package com.hidef.fc.dedicated.admin;
 
+import com.auth0.spring.security.auth0.Auth0TokenException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.GenericFilterBean;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @Component
-public class SimpleCORSFilter implements Filter {
+public class SimpleCORSFilter extends GenericFilterBean {
 
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
 
@@ -16,13 +22,9 @@ public class SimpleCORSFilter implements Filter {
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "Authorization");
+        response.setHeader("Access-Control-Allow-Headers", "Authorization, Cache-Control");
         chain.doFilter(req, res);
     }
-
-    public void init(FilterConfig filterConfig) {
-    }
-
-    public void destroy() {
-    }
 }
+
+
